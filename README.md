f7650c1d821ba15a2f41f7951ea0a67928ee1d00
friday27 <yiyinghahalin@gmail.com>
2019-03-23 17:07:00 +0800

    update -> 127. logical operator

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 <h2>The Web Developer Bootcamp</h2>
 
 Udemy course for beginners
+https://www.udemy.com/the-web-developer-bootcamp/learn/v4/overview
 
-Skills: NodeJS
+
+<h4>Skills</h4>
+
+* JavaScript
+* NodeJS
---
+++ experiments/JSBasics/Calc/script.js
@@ -0,0 +1,4 @@
+var userAge = prompt("How old are you!");
+var days = userAge / 4 * (365*4+1);
+alert("You know.. " + userAge + " years are roughly " + days + " days.");
+console.log(userAge + " years ~= " + days + " days.");
