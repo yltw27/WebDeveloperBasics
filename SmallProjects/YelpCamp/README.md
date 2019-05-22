085943d3e7da2dbb82356e9093e640c1665f6caa
friday27 <yiyinghahalin@gmail.com>
2019-05-22 22:57:41 +0800

    update 288-291 YelpCamp basic

---
+++ SmallProjects/YelpCamp/README.md
@@ -0,0 +1,15 @@
+# YelpCamp
+
+
+### Steps
+1. Add index page
+2. Add campgrounds page that lists all campgrounds
+    * Name
+    * Image
+3. Set up header and footer
+4. Install Bootstrap
+5. Add a feature for user to submit a new character
+
+
+### Resoruces
+* Photosforclass https://www.photosforclass.com/search
---
+++ SmallProjects/YelpCamp/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+
+var characters = [
+    {name: "Harry Potter", image: "https://farm8.staticflickr.com/7221/7305191686_25ca074567.jpg"},
+    {name: "Jon Snow", image: "https://farm9.staticflickr.com/8387/8565678298_644a2142b6.jpg"},
+    {name: "Rachel Green", image: "https://farm4.staticflickr.com/3675/10737623623_3b51721db6.jpg"},
+];
+
+app.get("/", function(req, res){
+    res.render("hello");
+});
+
+app.get("/characters", function(req, res){
+    res.render("characters",{characters: characters});
+});
+
+app.post("/characters", function(req, res){
+    characters.push({name: req.body.name, image: req.body.image});
+    res.redirect("/characters"); //default -> get
+});
+
+app.get("/characters/new", function(req, res){
+    res.render("new");
+});
+
+app.listen(3000, function(){
+    console.log("The YelpCamp server has started!");
+});
